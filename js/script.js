c86778331c768694977f949919e2775f234ffe41
devttheus <ttheus.rlima@gmail.com>
2022-09-06 16:03:22 -0300

    Adicionado duas estruturas de decisão, 'if'

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 var state = {board: [], currentGame: [], savedGame: []};
 
 function start(){
-    
+
+}
+
+function addNumbersToGame(numberToAdd){
+    if(numberToAdd < 1 || numberToAdd > 60){
+        console.error("Número invalido", numberToAdd);
+        return;
+    }
+
+    if(state.currentGame.length >= 6){
+        console.error("O jogo não aceita mais de 6 números");
+        return;
+    }
+    state.currentGame.push(numberToAdd);
 }
